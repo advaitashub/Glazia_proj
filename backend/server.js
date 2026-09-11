790cb23f757b1bcac0744f89ff22917ea094a4f1
advaitashub <singhadvaita95@gmail.com>
2026-09-12 00:25:34 +0530

    Connect frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
